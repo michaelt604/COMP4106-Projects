7129ccda185cc2e7a4c8678e13c2b13b69813d70
Michael Ting <michaelt604@gmail.com>
2021-02-16 16:43:35 -0500

    Update

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -1,7 +1,18 @@
 
-def main():
-    print("hi")
 
+
+# inputFileName contains a CSV file with the input grid
+# optimalPathFilename is the name of the file the optimal path should be written to
+# exploredListFilename is the name of the file the list of explored nodes should be written to
+def pathfinding(inputFileName, optimalPathFilename, exploredListFilename):
+    
+
+    return optimalPathCost
+
+
+def main():
+
+    return
 
 if (__name__ == "__main__"):
     main()
