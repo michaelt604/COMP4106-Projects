5c08f4aeb5e736b034eb24356ca667ca4db443ef
Margie Venes <margievenes@MacBook-Pro.local>
2021-02-19 02:03:32 -0500

    Display optimal path cost and path

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -3,6 +3,7 @@ import numpy as np
 import csv
 import math
 import sys
+from io import StringIO
 
 from queue import PriorityQueue
 
@@ -42,7 +43,9 @@ class Graph:
         self.nodes = {}
 
         self.S = (0, 0)
-        self.G = (0, 0)
+        #make an array of goal nodes
+        #self.G = (0, 0)
+        self.G = []
 
         self.createGraph(self.xCount, self.yCount, grid)  #Create our graph
     
@@ -53,7 +56,17 @@ class Graph:
             self.S = n
             return 0
         if (n.id == "G"):   #Check node end
-            self.G = n
+            
+            #print("n: "+ str(n.xy)) #checking for the x and y
+            if n not in self.G: #skip dupes
+                self.G.append(n) #add to the self.G array
+
+            '''
+            for iter in range(len(self.G)):
+                print("self.G[x].xy" + str(self.G[iter].xy))
+            '''
+            #print(self.G[1].xy)
+            #self.G = n
             return 0
         else:   #If a normal weighted node
             return int(n.id)    
@@ -93,7 +106,7 @@ class Graph:
 # exploredListFilename is the name of the file the list of explored nodes should be written to
 def pathfinding(inputFileName, optimalPathFilename, exploredListFilename):
     #Import CSV
-    csv = np.genfromtxt("Example1\input.csv", delimiter=",", dtype="str")
+    csv = np.genfromtxt("/Users/margievenes/Desktop/COMP 4106/A1/COMP4106-Projects/Assignment1/Example2/input.csv", delimiter=",", dtype="str")
     csv = np.char.strip(csv)
 
     print(csv)
@@ -105,42 +118,62 @@ def pathfinding(inputFileName, optimalPathFilename, exploredListFilename):
     g.nodes[g.S.xy[1], g.S.xy[0]].nCost = 0  #Start node cost at 0
     sNode = g.nodes[g.S.xy[1], g.S.xy[0]] #Physical start node
     pq.put((0, sNode))   #Priority queue start node
-    gNode = g.nodes[g.G.xy[1], g.G.xy[0]] #Physical goal node
-    
+    '''
+    Assuming this is just one goal node
+    a way to set multiple goal nodes 
+    store in an array?
+
+    gNode will refer to the array which will then access the coordinates
+    '''
+    #gNode = g.nodes[g.G.xy[1], g.G.xy[0]] #Physical goal node
+    #return the min value of a new list 
+
+    for index in range(len(g.G)):
+        print("gNode: " + str(g.G[index].xy))
     checked = []
-    
-
     #Pathfind
-    while (not pq.empty()):   
-        lowest = pq.get()   #Get lowest code node
 
-        #Explore node
-        lowest = lowest[1]    #Lowest node
-        checked.append(lowest.xy)   #Add to our checked list
-        print("Explore: " + str(lowest.xy) + " - Explored: " + str(checked))
-        neighbours = lowest.edges  #Nearby edges
+    #okay so store the key and value 
+    #key: path cost ->
+    #value: order ->
+    #print the minimum path at the end to the goal state
+    goalStatePath = {}
+    for index in range(len(g.G)):
+        gNode = g.nodes[g.G[index].xy[1], g.G[index].xy[0]] #Physical goal node
+    #^^^make sure the bottom code is nested within this for loop 
 
-        if (lowest.xy == gNode.xy):   #Reached end node     
-            print("REACHED END NODE")  
-            break
+        while (not pq.empty()):   
+            lowest = pq.get()   #Get lowest code node
 
-        for i in range(len(neighbours)):    #neighbours[i].n2 is the current node we're checking         
-            if (lowest.prev.xy == neighbours[i].n2.xy): #Prevents checking the node that this node came from
-                continue
-            estimate = 0    #No estimate yet
-            nodeCost = neighbours[i].cost + estimate     #Edge cost + heuristic estimate
-            neighbours[i].n2.checked = True
-            print("\tChecking: " + str(neighbours[i].n2.xy))
+            #Explore node
+            lowest = lowest[1]    #Lowest node
+            checked.append(lowest.xy)   #Add to our checked list
+            print("Explore: " + str(lowest.xy) + " - Explored: " + str(checked))
+            neighbours = lowest.edges  #Nearby edges
 
-            #Update node based on search
-            if (neighbours[i].n2.nCost > nodeCost):  #If nodes cost is larger than calculated, update node, add node to queue
-                neighbours[i].n2.nCost = nodeCost    #Update node cost
-                neighbours[i].n2.prev = lowest      #Set new route node (lowest cost to node)
+            if (lowest.xy == gNode.xy):   #Reached end node     
+                print("REACHED END NODE")  
+                break
 
-                pq.put((nodeCost, neighbours[i].n2))    #Add node to queue    
-                print("\t\tUpdating Cost From " + str(neighbours[i].n2.nCost) + " to " + str(nodeCost))
+            for i in range(len(neighbours)):    #neighbours[i].n2 is the current node we're checking         
+                if (lowest.prev.xy == neighbours[i].n2.xy): #Prevents checking the node that this node came from
+                    continue
+                heuristic = 0    #No heuristic yet
+                nodeCost = neighbours[i].cost + heuristic     #Edge cost + heuristic
+                neighbours[i].n2.checked = True
+                print("\tChecking: " + str(neighbours[i].n2.xy))
+
+                #Update node based on search
+                if (neighbours[i].n2.nCost > nodeCost):  #If nodes cost is larger than calculated, update node, add node to queue
+                    neighbours[i].n2.nCost = nodeCost    #Update node cost
+                    neighbours[i].n2.prev = lowest      #Set new route node (lowest cost to node)
+
+                    pq.put((nodeCost, neighbours[i].n2))    #Add node to queue    
+                    print("\t\tUpdating Cost From " + str(neighbours[i].n2.nCost) + " to " + str(nodeCost))
+        
+        print("done")
     
-    print("done")
+    pathCost(currentNode, startNode):
 
     finishedList = [gNode.xy]
     cost = 0
@@ -149,12 +182,19 @@ def pathfinding(inputFileName, optimalPathFilename, exploredListFilename):
     while(curNode.xy != sNode.xy):
         curNode = curNode.prev
         finishedList.append(curNode.xy)
+        print("before Cost = " + str(cost))
         cost += curNode.nCost
+        print("after Cost = " + str(cost))
 
     finishedList.reverse()
     print("Order = " + str(finishedList))
     print("Cost = " + str(cost))
+    goalStatePath[cost] = finishedList #add to the dictionary, key: cost, value: finishedList
 
+
+    minPathCostKey = min(goalStatePath.keys())
+    print("Minimum path cost KEY = " + str(minPathCostKey))
+    print("Minimum path cost VALUE = " + str(goalStatePath[minPathCostKey]))
 
     optimalPathCost = 0
 
