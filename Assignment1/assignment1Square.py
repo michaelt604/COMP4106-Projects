47832eb0d5e3f2e4b8b75856edb9434bf55bdc12
Michael Ting <michaelt604@gmail.com>
2021-03-19 19:07:14 -0400

    Working and tings

--- Assignment1/assignment1Square.py
+++ Assignment1/assignment1Square.py
@@ -62,48 +62,46 @@ class Graph:
             self.G.append(n)
             return 0
         else:   #If a normal weighted node
-            return float(n.id)    
+            return int(n.id)    
     
     def createGraph(self, xCount, yCount, grid):
-        for i in range(yCount): #Create all our nodes
-            for j in range(xCount):
-                gr = grid[i, j]
-                newNode = Node((j, i), gr)          #Create new node
+        for i in range(xCount): #Create all our nodes
+            for j in range(yCount):
+                newNode = Node((i, j), grid[i, j])          #Create new node
                 newNode.oriCost = self.getWeight(newNode)   #Original Node Cost
-                self.nodes[j, i] = newNode
+                self.nodes[i, j] = newNode
         
         for i in range(xCount):   #Create all our edges
             for j in range(yCount):   
-                node1 = self.nodes[i, j]
+                node1 = self.nodes[j, i]
                 n1Weight = self.getWeight(node1)  
 
                 if (n1Weight == -1):    #Empty node
                     continue
 
-                if (j+1 < yCount):    #If theres a node to the right of the current node, link them together                    
-                    node2 = self.nodes[i, j+1]
+                if (j + 1 < yCount):    #If theres a node to the right of the current node, link them together                    
+                    node2 = self.nodes[j+1, i]
                     n2Weight = self.getWeight(node2)                
                     if (not n2Weight == -1):    #Empty node
                         node1.addEdge(node2, n2Weight)    #Adds edge between nodes          
                         node2.addEdge(node1, n1Weight)    #Adds edge between nodes     
 
-                if (i+1 < xCount):    # if theres a node below the current node, link them together
-                    node2 = self.nodes[i+1, j]
+                if (i + 1 < xCount):    # if theres a node below the current node, link them together
+                    node2 = self.nodes[j, i+1]
                     n2Weight = self.getWeight(node2)                
                     if (not n2Weight == -1):    #Empty node
                         node1.addEdge(node2, n2Weight)    #Adds edge between nodes          
                         node2.addEdge(node1, n1Weight)    #Adds edge between nodes  
-                        
-        self.G = set(self.G)    #Removes goal duplicates
+                
         for i in range(xCount): #Set heuristic estimate for all nodes, chooses best heuristic based on closest goal
             for j in range(yCount):                
                 bestHuri = 0
                 for goal in self.G: #For all goals
-                    curNode = self.nodes[i, j]
-                    heuristic = (abs(curNode.xy[0] - goal.xy[0]) + abs(curNode.xy[1] - goal.xy[1])) * 1    #Manhattan distance estimate * average edge weight
+                    heuristic = (abs(self.nodes[j, i].xy[0] - goal.xy[0]) + abs(self.nodes[j, i].xy[1] - goal.xy[1])) * 1    #Manhattan distance estimate * average edge weight
                     bestHuri = min(bestHuri, heuristic) #Picks best current heuristic
 
-                self.nodes[i, j].heuri = bestHuri
+                self.nodes[j, i].heuri = bestHuri
+        self.G = set(self.G)    #Removes goal duplicates
 
 # inputFileName contains a CSV file with the input grid
 # optimalPathFilename is the name of the file the optimal path should be written to
@@ -111,7 +109,7 @@ class Graph:
 def pathfinding(inputFileName, optimalPathFilename, exploredListFilename):
     csv = np.genfromtxt(inputFileName, delimiter=",", dtype="str")      #Import CSV
     csv = np.char.strip(csv)    #Clean csv
-    csv = np.transpose(csv)     #Arrange array properly
+
     g = Graph(csv)  #Create Graph and Link nodes
     pq = PriorityQueue()    #Our priority queue
 
@@ -146,8 +144,8 @@ def pathfinding(inputFileName, optimalPathFilename, exploredListFilename):
         for i in range(len(neighbours)):    #neighbours[i].n2 is the current node we're checking         
             if (lowest.prev.xy == neighbours[i].n2.xy): #Prevents checking the node that this node came from
                 continue
- 
-            edgeCost = neighbours[i].cost   #Edge cost to next node
+
+            edgeCost = neighbours[i].cost   #Edge cost to next nude
             prevNodeCost = (lowestCost - lowest.heuri)  #True cost before next node
             nextNodeHeuristic = neighbours[i].n2.heuri  #Heuristic cost of next node
             nodeCost = prevNodeCost + edgeCost + nextNodeHeuristic     #Total node cost
@@ -162,25 +160,24 @@ def pathfinding(inputFileName, optimalPathFilename, exploredListFilename):
                 pq.put((nodeCost, neighbours[i].n2))    #Add node to queue    
     
 
-    optimalPath = [gNode.xy]
+    finishedList = [gNode.xy]
     cost = 0
 
     curNode = g.nodes[gNode.xy[0], gNode.xy[1]]
-    while(curNode.xy != sNode.xy):  #Loop through nodes on path to find optimal path
+    while(curNode.xy != sNode.xy):
         curNode = curNode.prev
-        #optimalPath.append(curNode.xy)
-        optimalPath.append((curNode.xy[0], curNode.xy[1]))  #Have to swap xy's due to some weird positional math going on since I coded for x being normal instead of x being y
+        finishedList.append(curNode.xy)
         cost += curNode.oriCost
 
-    optimalPath.reverse()
+    finishedList.reverse()
 
 
     #Write to files
-    optimalPathFile = open(optimalPathFilename, "w")
-    optimalPathFile.truncate()  #Cleans file before writing
-    for n in optimalPath:
-        optimalPathFile.write(str(n) + "\n")
-    optimalPathFile.close()
+    optimalPath = open(optimalPathFilename, "w")
+    optimalPath.truncate()  #Cleans file before writing
+    for n in finishedList:
+        optimalPath.write(str(n) + "\n")
+    optimalPath.close()
 
     exploredList = open(exploredListFilename, "w")
     exploredList.truncate()  #Cleans file before writing
@@ -197,7 +194,7 @@ def pathfinding(inputFileName, optimalPathFilename, exploredListFilename):
 
 
 def main():
-    cost = pathfinding("Assignment1/Example5/input.txt", "optimalpath.txt", "exploredList.txt")
+    cost = pathfinding("Assignment1/Example3/input.txt", "optimalpath.txt", "exploredList.txt")
 
     return
 
