3b194f11045f684c049ce6b67352587ac849275a
ignaciouss <34555148+ignaciouss@users.noreply.github.com>
2021-04-02 15:42:14 -0400

    Update FinalProject.py

--- FinalProject/FinalProject.py
+++ FinalProject/FinalProject.py
@@ -22,7 +22,7 @@ PAD_HEIGHT = 80
 HALF_PAD_WIDTH = round(PAD_WIDTH / 2)
 HALF_PAD_HEIGHT = round(PAD_HEIGHT / 2)
 ball_num = 0
-total_balls
+total_balls = 0
 balls=[]
 paddle1_vel = 0
 paddle2_vel = 0
@@ -39,46 +39,35 @@ class Ball:
 	#initialize balls in the center
 	def __init__(self,right,id):
 		self.id = id	
-		self.ball_pos = [WIDTH/2,HEIGHT/2] # center of screen
+		self.ball_pos = [random.randrange(0,WIDTH),random.randrange(0,HEIGHT)] # center of screen
 		horz = random.randrange(2,4)
 		vert = random.randrange(1,3)
-		
+		self.colour =  [random.randrange(1,254), random.randrange(1,254), random.randrange(1,254)]  # different colour for each ball
 		if right == False:  
 			horz = - horz   # shoot ball left if variable is false
 			
 		self.ball_vel = [horz,-vert]  # ball starts shooting downward
 
-	# reset balls to shoot from middle to opposite side of goal
-	def resetBall(self,right):
-		self.ball_pos = [WIDTH/2,HEIGHT/2] # reset to the center
-		horz = random.randrange(2,4)
-		vert = random.randrange(1,3)
-		ball_num += 1
-		
-		if right == False:  
-			horz = - horz   # shoot ball left if variable is false
-		self.ball_vel = [horz,-vert]
-
 
 # define event handlers
 def init():
-    global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel,l_score,r_score,total_balls,balls,ball_num # these are floats
-    global score1, score2  # these are ints
+    global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel,l_score,r_score# these are floats
+    global score1, score2,total_balls,balls,ball_num   # these are ints
     paddle1_pos = [round(HALF_PAD_WIDTH - 1), round(HEIGHT/2)]
     paddle2_pos = [round(WIDTH +1 - HALF_PAD_WIDTH), round(HEIGHT/2)]
     l_score = 0
     r_score = 0
-	total_balls = 1
-	balls = []
-	ball_num = total_balls
+    total_balls = 2   #<<<<<<<<<<<<<<  HOW YOU CHNAGE TOTAL NUMBER OF BALLS 
+    balls = []
+    ball_num = total_balls
    
    #initilize list of balls
     if random.randrange(0,2) == 0:
         for x in range(total_balls):
-			balls.append(Ball(True,x))
+            balls.append(Ball(True,x))
     else:
         for x in range(total_balls):
-			balls.append(Ball(False,x))
+            balls.append(Ball(False,x))
 
 
 #draw function of canvas
@@ -107,52 +96,55 @@ def draw(canvas):
         paddle2_pos[1] += paddle2_vel
 
     #update balls
-	for pongBall in balls:
-		pongBall.ball_pos[0] += round(pongBall.ball_vel[0])
-		pongBall.ball_pos[1] += round(pongBall.ball_vel[1])
+    for pongBall in balls:
+        pongBall.ball_pos[0] += round(pongBall.ball_vel[0])
+        pongBall.ball_pos[1] += round(pongBall.ball_vel[1])
 
     #draw paddles and ball
-	 for pongball in balls: 
-		colour =  [random.randrange(1,254), random.randrange(1,254), random.randrange(1,254)]  # different colour for each ball
-		pygame.draw.circle(canvas, colour, pongball.ball_pos, 20, 0)
+    for pongball in balls: 
+        pygame.draw.circle(canvas, pongball.colour, pongball.ball_pos, 20, 0)
     pygame.draw.polygon(canvas, GREEN, [[paddle1_pos[0] - HALF_PAD_WIDTH, paddle1_pos[1] - HALF_PAD_HEIGHT], [paddle1_pos[0] - HALF_PAD_WIDTH, paddle1_pos[1] + HALF_PAD_HEIGHT], [paddle1_pos[0] + HALF_PAD_WIDTH, paddle1_pos[1] + HALF_PAD_HEIGHT], [paddle1_pos[0] + HALF_PAD_WIDTH, paddle1_pos[1] - HALF_PAD_HEIGHT]], 0)
     pygame.draw.polygon(canvas, GREEN, [[paddle2_pos[0] - HALF_PAD_WIDTH, paddle2_pos[1] - HALF_PAD_HEIGHT], [paddle2_pos[0] - HALF_PAD_WIDTH, paddle2_pos[1] + HALF_PAD_HEIGHT], [paddle2_pos[0] + HALF_PAD_WIDTH, paddle2_pos[1] + HALF_PAD_HEIGHT], [paddle2_pos[0] + HALF_PAD_WIDTH, paddle2_pos[1] - HALF_PAD_HEIGHT]], 0)
 
     #ball collision check on top and bottom walls
     for pongBall in balls:
-		if round(pongBall.ball_pos[1]) <= BALL_RADIUS:
-        	pongBall.ball_vel[1] = - pongBall.ball_vel[1]
-    	if round(pongBall.ball_pos[1]) >= HEIGHT + 1 - BALL_RADIUS:
+        if round(pongBall.ball_pos[1]) <= BALL_RADIUS:
+            pongBall.ball_vel[1] = - pongBall.ball_vel[1]
+        if round(pongBall.ball_pos[1]) >= HEIGHT + 1 - BALL_RADIUS:
         	pongBall.ball_vel[1] = -pongBall.ball_vel[1]
     
     #ball collison check on gutters or paddles
-	r_goal = False
-    for pongball in balls:	
-		if round(pongball.ball_pos[0]) <= BALL_RADIUS + PAD_WIDTH and round(pongball.ball_pos[1]) in range(paddle1_pos[1] - HALF_PAD_HEIGHT,paddle1_pos[1] + HALF_PAD_HEIGHT,1):
-			pongball.ball_vel[0] = -ball_vel[0]
-			pongball.ball_vel[0] *= 1.1
-			pongball.ball_vel[1] *= 1.1
-		elif round(pongball.ball_pos[0]) <= BALL_RADIUS + PAD_WIDTH:
-			r_score += 1
-			ball_num -= 1
-			r_goal = True	
+    r_goal = False
+    for pongball in balls:
+        if round(pongball.ball_pos[0]) <= BALL_RADIUS + PAD_WIDTH and round(pongball.ball_pos[1]) in range(paddle1_pos[1] - HALF_PAD_HEIGHT,paddle1_pos[1] + HALF_PAD_HEIGHT,1):
+            pongball.ball_vel[0] = -pongball.ball_vel[0]
+            pongball.ball_vel[0] *= 1.1
+            pongball.ball_vel[1] *= 1.1
+        elif round(pongball.ball_pos[0]) <= BALL_RADIUS + PAD_WIDTH:  
+            # when scored on left side, increase score remove ball from list of balls
+            r_score += 1
+            ball_num -= 1
+            r_goal = True
+            balls.remove(pongball)	
 			
 	
-	for pongball in balls:	
-		if round(pongball.ball_pos[0]) >= WIDTH + 1 - BALL_RADIUS - PAD_WIDTH and round(pongball.ball_pos[1]) in range(paddle2_pos[1] - HALF_PAD_HEIGHT,paddle2_pos[1] + HALF_PAD_HEIGHT,1):
-			pongball.ball_vel[0] = -ball_vel[0]
-			pongball.ball_vel[0] *= 1.1
-			pongball.ball_vel[1] *= 1.1
-		elif round(pongball.ball_pos[0]) >= WIDTH + 1 - BALL_RADIUS - PAD_WIDTH:
-			l_score += 1
-			ball_num -= 1
-			r_goal = False
+    for pongball in balls:
+        if round(pongball.ball_pos[0]) >= WIDTH + 1 - BALL_RADIUS - PAD_WIDTH and round(pongball.ball_pos[1]) in range(paddle2_pos[1] - HALF_PAD_HEIGHT,paddle2_pos[1] + HALF_PAD_HEIGHT,1):
+            pongball.ball_vel[0] = -pongball.ball_vel[0]
+            pongball.ball_vel[0] *= 1.1
+            pongball.ball_vel[1] *= 1.1
+        elif round(pongball.ball_pos[0]) >= WIDTH + 1 - BALL_RADIUS - PAD_WIDTH:
+            # when scored on right side, increase score remove ball from list of balls
+            l_score += 1
+            ball_num -= 1
+            r_goal = False
+            balls.remove(pongball)
 	
-	#If no more balls in play, reset them all in center
-	if ball_num = 0: 
-		for pongball in balls:
-			pongball.resetBall(r_goal)
-		ball_num = total_balls
+	#If no more balls in play, reset game by remaking total number of balls
+    if ball_num == 0:
+        for x in range(total_balls):    
+            balls.append(Ball(r_goal,x)) # remake total number of balls
+        ball_num = total_balls
     
 	#update scores
     myfont1 = pygame.font.SysFont("Comic Sans MS", 20)
