68e466f05c98c64dd51526f46af310fa5ec3c8d8
Michael Ting <michaelt604@gmail.com>
2021-04-13 15:46:02 -0400

    Fixed alot of issues, moved closer to how we will want it in the future

--- FinalProject/FinalProject.py
+++ FinalProject/FinalProject.py
@@ -32,67 +32,46 @@ BALL_RADIUS = 20
 ball_num = 0
 total_balls = 0
 balls = []
-paddle1_vel = 0
-paddle2_vel = 0
+paddles = []
+
 l_score = 0
 r_score = 0
 
-class Paddle1:
+paddle1 = 0
+paddle2 = 0
+
+class Paddle:
     """
     Class to keep track of a ball's location and vector.
     """
-    def __init__(self):
-        self.x = 0
-        self.y = 0
+    def __init__(self, id, colour, xStart, yStart, control):
+        self.id = id
+        self.colour = colour
+        self.x = xStart
+        self.y = yStart
         self.change_x = 0
         self.change_y = 0
+        self.control = control
+        self.moveVel = 8
 
- 
-    def make_paddle():
-        """
-        Function to make a new, random ball.
-        """
-        paddle = Paddle1()
-        # Starting position of the paddle.
-        # Take into account the ball size so we don't spawn on the edge.
-        paddle.x = 0 #x coordinate where the ball will be
-        paddle.y = 100 #where the y will be for the ball coordinates
-        #paddle.y = 0
+    def movePaddle(self, balls):    #AI Handling of the ball movement
+        if (self.control == "AI"):
+            if (abs(balls[0].final_pos[1] - self.y) <= 8):  #If within distance of point, stop moving
+                self.change_y = 0   
+            elif (balls[0].final_pos[1] > self.y):
+                self.change_y = self.moveVel
+            elif (balls[0].final_pos[1] < self.y):
+                self.change_y = -self.moveVel
 
-        # Speed and direction of paddle 
-        paddle.change_x = random.randrange(1, 3)
-        paddle.change_y = random.randrange(1, 3)
-    
-        return paddle
-    
+            print(f"Position: {self.y} - Goal {balls[0].final_pos[1]}")
 
-class Paddle2:
-    """
-    Class to keep track of a ball's location and vector.
-    """
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.change_x = 0
-        self.change_y = 0
-
- 
-    def make_paddle():
-        """
-        Function to make a new, random ball.
-        """
-        paddle = Paddle2()
-        # Starting position of the paddle.
-        # Take into account the ball size so we don't spawn on the edge.
-        paddle.x = 0 #x coordinate where the ball will be
-        paddle.y = 0 #where the y will be for the ball coordinates
-        #paddle.y = 0
-
-        # Speed and direction of paddle 
-
-        paddle.change_y = 2
-    
-        return paddle
+        # Bounce the paddle if needed
+        if (self.y > HALF_PAD_HEIGHT and self.y < SCREEN_HEIGHT - HALF_PAD_HEIGHT):
+            self.y += self.change_y
+        elif (self.y == HALF_PAD_HEIGHT and self.change_y > 0):
+            self.y += self.change_y
+        elif (self.y == SCREEN_HEIGHT - HALF_PAD_HEIGHT and self.change_y < 0):
+            self.y += self.change_y
 
 class Ball:
     # initialize balls in the center
@@ -132,23 +111,15 @@ def init():
     # these are floats
     
     global l_score, r_score, paddle1_pos
-    global score1, score2, total_balls, balls, ball_num   # these are ints
-    paddle1_pos = [round(HALF_PAD_WIDTH - 1), round(SCREEN_HEIGHT/2)]
-    '''
-    paddle1_pos = [round(HALF_PAD_WIDTH - 1), round(HEIGHT/2)]
-    paddle2_pos = [round(WIDTH + 1 - HALF_PAD_WIDTH), round(HEIGHT/2)]
-    
-    paddle1.x = round(HALF_PAD_WIDTH - 1)
-    paddle1.y = round(HEIGHT/2)
+    global score1, score2, total_balls, balls, ball_num   # these are ints    
 
-    paddle2.x = round(WIDTH + 1 - HALF_PAD_WIDTH)
-    paddle2.y = round(HEIGHT/2)
-    '''
     l_score = 0
     r_score = 0
     total_balls = 1  # <<<<<<<<<<<<<<  HOW YOU CHNAGE TOTAL NUMBER OF BALLS
     balls = []
     ball_num = total_balls
+
+    #Initialize the paddles
 
     # initilize list of balls
     if random.randrange(0, 2) == 0:
@@ -160,38 +131,35 @@ def init():
 
 # keydown handler
 def keydown(event):
-    global paddle1_vel, paddle2_vel
+    for paddle in paddles:
+        if (paddle.id == 1 and paddle.control == "Player"):
+            if (event.key == K_w):
+                paddle.change_y = -8
+            elif (event.key == K_s):
+                paddle.change_y = 8
+        elif (paddle.id == 2 and paddle.control == "Player"):
+            if (event.key == K_UP):
+                paddle.change_y = -8
+            elif (event.key == K_DOWN):
+                paddle.change_y = 8
 
-    if event.key == K_UP:
-        paddle1_vel = -8
-        #paddle1.change_y = -8
-    elif event.key == K_DOWN:
-        paddle1_vel = 8
-        #paddle1.change_y = 8
-    '''
-    elif event.key == K_w:
-        paddle1_vel = -8
-    elif event.key == K_s:
-        paddle1_vel = 8
-    '''
 # keyup handler
 def keyup(event):
-    global paddle1_vel, paddle2_vel
+    for paddle in paddles:
+        if (paddle.id == 1 and paddle.control == "Player"):
+            if (event.key in (K_w, K_s)):
+                paddle.change_y = 0
+        if (paddle.id == 2 and paddle.control == "Player"):
+            if (event.key in (K_UP, K_DOWN)):
+                paddle.change_y = 0
 
-    if event.key in (K_UP, K_DOWN):
-        #paddle1.change_y = 0
-        paddle1_vel = 0
-    '''
-    elif event.key in (K_UP, K_DOWN):
-        paddle2_vel = 0
-    '''
 
 def main():
     """
     This is our main program.
     """
     pygame.init()
-    global paddle1_pos, paddle2_pos, l_score, r_score, ball_num, total_balls, balls, ball_num, paddle1_vel
+    global l_score, r_score, ball_num, total_balls, balls, paddles, ball_num, paddle1_vel, paddle1, paddle2
     # Set the height and width of the screen
     init()
     size = [SCREEN_WIDTH, SCREEN_HEIGHT]
@@ -205,8 +173,13 @@ def main():
     # Used to manage how fast the screen updates
     clock = pygame.time.Clock()
     
-    paddle1 = Paddle1.make_paddle() #calls the make paddle function for the x and y's
-    paddle2 = Paddle2.make_paddle() 
+    paddles.append(Paddle(1, GREEN, round(HALF_PAD_WIDTH - 1), round(SCREEN_HEIGHT/2), "Player"))      #Paddle 1
+    paddles.append(Paddle(2, RED, round(SCREEN_WIDTH + 1 - HALF_PAD_WIDTH), round(SCREEN_HEIGHT/2), "AI"))    #Paddle 2
+
+
+    print("start main loop")
+
+
     # -------- Main Program Loop -----------
     while not done:
             #elif event.type == pygame.KEYDOWN:
@@ -215,48 +188,16 @@ def main():
               #      paddle = make_paddle()
                #     paddle_list.append(paddle)
  
-        # --- Logic
-        #for paddle in paddle_list:
-            # Move the paddle's center
-        #paddle1.x += paddle1.change_x
-        #paddle1.y += paddle1.change_y
 
-        if (balls[0].final_pos[1] > paddle2.y):
-            paddle2.y += paddle2.change_y
-        elif (balls[0].final_pos[1] < paddle2.y):
-            paddle2.y -= paddle2.change_y
+        '''Move Game Entities'''
+        for pongBall in balls:  #Move balls
+            pongBall.ball_pos[0] += round(pongBall.ball_vel[0])
+            pongBall.ball_pos[1] += round(pongBall.ball_vel[1])
 
-        #paddle2.x += paddle2.change_x
-        #paddle2.y += paddle2.change_y
+        for paddle in paddles:  #Move paddles
+            paddle.movePaddle(balls)
 
-        # Bounce the paddle if needed
-        if paddle1_pos[1] > HALF_PAD_HEIGHT and paddle1_pos[1] < SCREEN_HEIGHT - HALF_PAD_HEIGHT:
-            paddle1_pos[1] += paddle1_vel
-        elif paddle1_pos[1] == HALF_PAD_HEIGHT and paddle1_vel > 0:
-            paddle1_pos[1] += paddle1_vel
-        elif paddle1_pos[1] == SCREEN_HEIGHT - HALF_PAD_HEIGHT and paddle1_vel < 0:
-            paddle1_pos[1] += paddle1_vel
-        
-        '''
-        if paddle2.x > HALF_PAD_HEIGHT and paddle1_pos[1] < SCREEN_HEIGHT - HALF_PAD_HEIGHT:
-            paddle2.x += paddle1_vel
-        elif paddle2.x == HALF_PAD_HEIGHT and paddle1_vel > 0:
-            paddle2.x += paddle1_vel
-        elif paddle2.x == SCREEN_HEIGHT - HALF_PAD_HEIGHT and paddle1_vel < 0:
-            paddle2.x += paddle1_vel
-        '''
-        '''
-        if paddle1.y > SCREEN_HEIGHT - PAD_HEIGHT or paddle1.y < 0:
-            paddle1.change_y *= -1
-        if paddle1.x > SCREEN_WIDTH  - PAD_WIDTH or paddle1.x < PAD_WIDTH:
-            paddle1.change_x *= -1
-        '''
 
-        #if paddle2.y > SCREEN_HEIGHT - PAD_HEIGHT or paddle2.y < 0:
-        #    paddle2.change_y *= -1
-        #if paddle2.x > SCREEN_WIDTH  - PAD_WIDTH or paddle2.x < PAD_WIDTH:
-        #    paddle2.change_x *= -1
- 
         # --- Drawing
         # Set the screen background
         screen.fill(BLACK)
@@ -264,19 +205,16 @@ def main():
         pygame.draw.line(screen, WHITE, [PAD_WIDTH, 0], [PAD_WIDTH, SCREEN_HEIGHT], 1)
         pygame.draw.line(screen, WHITE, [SCREEN_WIDTH - PAD_WIDTH, 0], [SCREEN_WIDTH - PAD_WIDTH, SCREEN_HEIGHT], 1)
         pygame.draw.circle(screen, WHITE, [SCREEN_WIDTH//2, SCREEN_HEIGHT//2], 70, 1)
-        # Draw the paddles
-        #for paddle in paddle_list:
-            #pygame.draw.circle(screen, WHITE, [paddle.x, paddle.y], paddle_SIZE)
-        #pygame.draw.rect(screen, RED, pygame.Rect(paddle1.x, paddle1.y, PAD_WIDTH, PAD_HEIGHT))
-        pygame.draw.polygon(screen, RED, [[paddle1_pos[0] - HALF_PAD_WIDTH, paddle1_pos[1] - HALF_PAD_HEIGHT], [paddle1_pos[0] - HALF_PAD_WIDTH, paddle1_pos[1] + HALF_PAD_HEIGHT], [paddle1_pos[0] + HALF_PAD_WIDTH, paddle1_pos[1] + HALF_PAD_HEIGHT], [paddle1_pos[0] + HALF_PAD_WIDTH, paddle1_pos[1] - HALF_PAD_HEIGHT]], 0)
-        pygame.draw.rect(screen, GREEN, pygame.Rect(SCREEN_WIDTH - PAD_WIDTH, paddle2.y, PAD_WIDTH, PAD_HEIGHT))
-        for pongBall in balls:
-            pongBall.ball_pos[0] += round(pongBall.ball_vel[0])
-            pongBall.ball_pos[1] += round(pongBall.ball_vel[1])
+        
 
-    # draw balls and paddles
+        # draw balls 
         for pongball in balls:
             pygame.draw.circle(screen, pongball.colour, pongball.ball_pos, 20, 0)
+
+        #Draw the paddles    
+        for paddle in paddles:
+            pygame.draw.polygon(screen, paddle.colour, [[paddle.x - HALF_PAD_WIDTH, paddle.y - HALF_PAD_HEIGHT], [paddle.x - HALF_PAD_WIDTH, paddle.y + HALF_PAD_HEIGHT], [paddle.x + HALF_PAD_WIDTH, paddle.y + HALF_PAD_HEIGHT], [paddle.x + HALF_PAD_WIDTH, paddle.y - HALF_PAD_HEIGHT]], 0)
+
 
         # ball collision check on top and bottom walls
         for pongBall in balls:
@@ -284,20 +222,19 @@ def main():
                 pongBall.ball_vel[1] = -pongBall.ball_vel[1]
             if round(pongBall.ball_pos[1]) >=SCREEN_HEIGHT + 1 - BALL_RADIUS:
                 pongBall.ball_vel[1] = -pongBall.ball_vel[1]
-            # ball collison check on gutters or paddles
+        
+        # ball collison check on gutters or paddles
         r_goal = False
         for pongball in balls:
-            #leftSide = round(pongball.ball_pos[0]) <= BALL_RADIUS + PAD_WIDTH and round(pongball.ball_pos[1]) in range(paddle1.y - HALF_PAD_HEIGHT, paddle1.y + HALF_PAD_HEIGHT, 1)
-            leftSide = round(pongball.ball_pos[0]) <= BALL_RADIUS + PAD_WIDTH and round(pongball.ball_pos[1]) in range(paddle1_pos[1] - HALF_PAD_HEIGHT, paddle1_pos[1] + HALF_PAD_HEIGHT, 1)
-            rightSide = round(pongball.ball_pos[0]) >=SCREEN_WIDTH + 1 - BALL_RADIUS - PAD_WIDTH and round(pongball.ball_pos[1]) in range(paddle2.y - HALF_PAD_HEIGHT, paddle2.y + HALF_PAD_HEIGHT, 1)
-
+            leftSide = round(pongball.ball_pos[0]) <= BALL_RADIUS + PAD_WIDTH and round(pongball.ball_pos[1]) in range(paddles[0].y - HALF_PAD_HEIGHT, paddles[0].y + HALF_PAD_HEIGHT, 1)
+            rightSide = round(pongball.ball_pos[0]) >=SCREEN_WIDTH + 1 - BALL_RADIUS - PAD_WIDTH and round(pongball.ball_pos[1]) in range(paddles[1].y - HALF_PAD_HEIGHT, paddles[1].y + HALF_PAD_HEIGHT, 1)
+            
             score = False
             if (leftSide or rightSide):
                 pongball.ball_vel[0] = -pongball.ball_vel[0]
                 pongball.ball_vel[0] *= 1.1
                 pongball.ball_vel[1] *= 1.1
                 pongball.bouncePosition()
-                #paddle2.y = pongball.final_pos[1]
 
             elif round(pongball.ball_pos[0]) <= BALL_RADIUS + PAD_WIDTH:    # when scored on left side, increase score remove ball from list of balls        
                 score = True
@@ -314,15 +251,7 @@ def main():
                 #paddle2.y = pongball.final_pos[1]
                 print(f"FinalPos: {pongball.ball_pos}, CalcFinalPos: {pongball.final_pos}")
             
-            #after ball estimation this changes paddle 2's location
-            '''
-            if paddle2.y > SCREEN_HEIGHT - PAD_HEIGHT or paddle2.y < 0:
-                paddle2.change_y *= -1
-                paddle2.y = pongball.final_pos[1]
-    
-            if paddle2.x > SCREEN_WIDTH  - PAD_WIDTH or paddle2.x < PAD_WIDTH:
-                paddle2.change_x *= -1
-            '''
+ 
         # If no more balls in play, reset game by remaking total number of balls
         if ball_num == 0:
             for x in range(total_balls):
@@ -337,9 +266,8 @@ def main():
         myfont2 = pygame.font.SysFont("Comic Sans MS", 20)
         label2 = myfont2.render("Score "+str(r_score), 1, (255, 255, 0))
         screen.blit(label2, (470, 20))
-        # --- Wrap-up
-        # Limit to 60 frames per second
-        # --- Event Processing
+
+        # --- Wrap-up :: Limit to 60 frames per second :: Event Processing --- 
         for event in pygame.event.get():
             if event.type == KEYDOWN:
                 keydown(event)
